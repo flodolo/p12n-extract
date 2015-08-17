d6d29d3ab93cc0ac22663323add9418e0c9a6b66
Francesco Lodolo (:flod) <flod@lodolo.net>
2015-08-17 11:36:15 +0200

    Add script to check URLs HTTP response

---
+++ utils/check_urls.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import json
+import os
+import re
+import urllib2
+
+
+json_file = 'searchplugins.json'
+json_data = json.load(open(json_file))
+
+products = ['browser', 'mobile']
+errors = []
+
+locales = []
+for locale in json_data['locales']:
+    locales.append(locale)
+locales.sort()
+
+for locale in locales:
+    print "\n\n------\nAnalyzing %s..." % locale
+    for product in products:
+        if product in json_data['locales'][locale] and 'aurora' in json_data['locales'][locale][product]:
+            searchplugins = json_data['locales'][locale][product]['aurora']['searchplugins']
+            for searchplugin in searchplugins:
+                if '(en-US)' not in searchplugins[searchplugin]['description']:
+                    print 'Checking %s' % searchplugins[searchplugin]['file']
+                    url = searchplugins[searchplugin]['url']
+                    try:
+                        response = urllib2.urlopen(url.encode('UTF-8'))
+                    except urllib2.HTTPError as e:
+                        errors.append('[%s - %s] Response: %s - URL: %s' % (locale, product, e.code, url))
+                    except urllib2.URLError as e:
+                        errors.append('[%s - %s] Response: %s - URL: %s' % (locale, product, e.reason, url))
+                    except Exception as e:
+                        errors.append('[%s - %s] Response: %s - URL: %s' % (locale, product, e.code, url))
+
+if errors:
+    print '\n'.join(errors)
+
