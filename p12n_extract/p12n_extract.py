aee6974d357402abc7b542585c85e374e85fd82e
Francesco Lodolo <flod@lodolo.net>
2016-10-13 12:34:26 +0200

    Add centralized list.json for Thunderbird (#10)

--- p12n_extract/p12n_extract.py
+++ p12n_extract/p12n_extract.py
@@ -83,8 +83,11 @@ class ProductizationData():
                     try:
                         with open(centralized_source) as data_file:
                             centralized_json = json.load(data_file)
-                        list_sp = centralized_json['locales'][locale][
-                            'default']['visibleDefaultEngines']
+                        if locale in centralized_json['locales']:
+                            list_sp = centralized_json['locales'][locale][
+                                'default']['visibleDefaultEngines']
+                        else:
+                            errors.append('locale is not defined in list.json')
                     except Exception as e:
                         print e
                 else:
@@ -536,9 +539,6 @@ class ProductizationData():
                     'mail': os.path.join(base, 'mail', 'locales', 'en-US', 'en-US', 'searchplugins'),
                     'suite': os.path.join(base, 'suite', 'locales', 'en-US', 'en-US', 'searchplugins')
                 },
-                'sp_centralized': {
-                    'browser': os.path.join(channel_data['centralized_source'], 'browser', 'locales', 'search', 'list.json'),
-                },
                 'p12n': {
                     'browser': [os.path.join(base, 'browser', 'locales', 'en-US', 'en-US', 'chrome', 'browser-region', 'region.properties')],
                     'mobile': [os.path.join(base, 'mobile', 'locales', 'en-US', 'en-US', 'chrome', 'region.properties')],
@@ -557,8 +557,20 @@ class ProductizationData():
                 if requested_product in ['all', product]:
                     # Analyze en-US first
                     path_enUS = search_path_enUS['sp'][product]
-                    path_centralized = search_path_enUS[
-                        'sp_centralized'].get(product, '')
+
+                    # Define path to centralized list.json
+                    path_centralized = ''
+                    if product == 'browser':
+                        repo_folder = 'mozilla-central' if requested_channel == 'trunk' else 'mozilla-{0}'.format(
+                            requested_channel)
+                        path_centralized = os.path.join(
+                            channel_data['source_path'], repo_folder, 'browser', 'locales', 'search', 'list.json')
+                    elif product == 'mail':
+                        repo_folder = 'comm-central' if requested_channel == 'trunk' else 'comm-{0}'.format(
+                            requested_channel)
+                        path_centralized = os.path.join(
+                            channel_data['source_path'], repo_folder, 'mail', 'locales', 'search', 'list.json')
+
                     self.extract_splist_enUS(
                         path_centralized, path_enUS, product,
                         requested_channel)
@@ -636,7 +648,8 @@ class ProductizationData():
 def main():
     # Parse command line options
     cl_parser = argparse.ArgumentParser()
-    cl_parser.add_argument('config_folder', help='Path to Transvision /config folder')
+    cl_parser.add_argument(
+        'config_folder', help='Path to Transvision /config folder')
     cl_parser.add_argument('-p', '--product', help='Choose a specific product',
                            choices=['browser', 'mobile', 'mail', 'suite', 'all'], default='all')
     cl_parser.add_argument('-b', '--branch', help='Choose a specific branch',
@@ -651,7 +664,8 @@ def main():
     # ../config from current script location (not current folder). Store all
     # needed folders in vars.
     parser = SafeConfigParser()
-    transvision_config = os.path.abspath(os.path.join(args.config_folder, 'config.ini'))
+    transvision_config = os.path.abspath(
+        os.path.join(args.config_folder, 'config.ini'))
     if not os.path.isfile(transvision_config):
         print "config.ini not found in {0}".format(args.config_folder)
         sys.exit(1)
@@ -665,13 +679,10 @@ def main():
         if args.branch in ['all', channel]:
             source_name = 'central.txt' if channel == 'trunk' else '{0}.txt'.format(
                 channel)
-            repo_folder = 'mozilla-central' if channel == 'trunk' else 'mozilla-{0}'.format(
-                channel)
             channel_data = {
                 'l10n_path': os.path.join(local_hg, '{0}_L10N'.format(channel.upper())),
                 'locales_file': os.path.join(config_files, source_name),
-                'source_path': os.path.join(local_hg, '{0}_EN-US'.format(channel.upper())),
-                'centralized_source': os.path.join(local_hg, '{0}_EN-US'.format(channel.upper()), repo_folder),
+                'source_path': os.path.join(local_hg, '{0}_EN-US'.format(channel.upper()))
             }
             p12n.extract_p12n_channel(
                 args.product, channel_data, channel, args.noproductization)
